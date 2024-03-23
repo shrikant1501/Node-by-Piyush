16ae96a635d5955da5215404c1d368613658c907
Shrikant <thakresrikant@gmail.com>
2024-03-23 15:01:32 +0530

    basic node routing

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const http = require("http");
 
 const myServer = http.createServer((req, res) => {
-  console.log("New request came");
-  res.end("hello from server");
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello World!");
+  } else if (req.url === "/about") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("About Us Page");
+  } else if (req.url === "/contact") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Contact Us Page");
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Page Not Found");
+  }
 });
 
 myServer.listen(8000, () => console.log("Sever Started!"));
